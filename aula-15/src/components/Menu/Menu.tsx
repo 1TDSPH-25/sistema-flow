2ebd6a585126a0e12008e64a06956fcc7f8a8be1
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-09-26 10:56:51 -0300

    Estilizando a barra de navegação

--- aula-15/src/components/Menu/Menu.tsx
+++ aula-15/src/components/Menu/Menu.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Menu(){
     return(
-        <nav>
-            <Link to="/">Home</Link>|
-            <Link to="/produtos">Produtos</Link> |
-            <Link to="/card">Cards</Link> |
+        <nav className="menu">
+            <Link to="/">Home</Link>
+            <Link to="/produtos">Produtos</Link>
+            <Link to="/card">Cards</Link>
             <Link to="/git-users">Git Usuários</Link>
         </nav>
     );
