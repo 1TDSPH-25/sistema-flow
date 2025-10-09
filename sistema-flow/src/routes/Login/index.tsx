57c474b68db804b91716daea9b1015becfca577c
Felipe-M-de-Sousa <felipe.sousa.1106@gmail.com>
2025-10-09 10:04:29 -0300

    docs(menu): adicionando lembrete para devs sobre rotas que devem ser criadas no menu.

---
+++ sistema-flow/src/routes/Login/index.tsx
@@ -0,0 +1,6 @@
+/* Adicionado lembrete no código informando aos devs do menu quais rotas deverão ser criadas e adicionadas ao menu principal:
+- Login
+- Cadastro
+
+Outras rotas só devem ser implementadas mediante definição oficial nas tarefas
+para manter o escopo controlado da feature atual. */ 
