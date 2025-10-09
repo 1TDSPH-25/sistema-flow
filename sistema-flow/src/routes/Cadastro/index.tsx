792c4dd7876610eb1a3ce76631ff4e8d30deda8b
Felipe-M-de-Sousa <felipe.sousa.1106@gmail.com>
2025-10-09 10:15:53 -0300

    docs(menu): atualizando mensagem de aviso para os devs sobre criação de rotas.

--- sistema-flow/src/routes/Cadastro/index.tsx
+++ sistema-flow/src/routes/Cadastro/index.tsx
@@ -1,6 +1,7 @@
-/* Adicionado lembrete no código informando aos devs do menu quais rotas deverão ser criadas e adicionadas ao menu principal:
-- Login
-- Cadastro
+/* Adicionando lembrete no código informando aos devs do menu quais rotas deverão ser criadas e adicionadas ao menu principal:
+scr|
+   | routes |
+            | Login - index.tsx
+            | Cadastro - index.tsx
 
-Outras rotas só devem ser implementadas mediante definição oficial nas tarefas
-para manter o escopo controlado da feature atual. */ 
+Outras rotas só devem ser implementadas mediante definição oficial nas tarefas para manter o escopo designado pelo professor. */ 
