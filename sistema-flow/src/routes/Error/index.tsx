e5a724fd43e8dff668d4b6b8dad32f99049580e9
lunaguima <luna.dcarvalho05@gmail.com>
2025-10-13 21:17:56 -0300

    feat: rota de erro adicionada

---
+++ sistema-flow/src/routes/Error/index.tsx
@@ -0,0 +1,15 @@
+/* Adicionando lembrete no código informando aos devs do menu quais rotas deverão ser criadas e adicionadas ao menu principal:
+scr|
+   | routes |
+            | Login - index.tsx
+            | Cadastro - index.tsx
+
+Outras rotas só devem ser implementadas mediante definição oficial nas tarefas para manter o escopo designado pelo professor. */ 
+
+export default function Error(){
+   return(
+      <main>
+        <h1>Not Found - Erro 404</h1>
+      </main>
+   );
+}
