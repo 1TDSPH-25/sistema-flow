ca18e1ac3a1080d3e5939e0798948b3f1a8fcdbc
manuelalacerda <manuelalacerda88@gmail.com>
2025-10-15 20:57:37 -0300

    feat: Removendo estilização base da rota Remedio.

--- sistema-flow/src/routes/Remedios/index.tsx
+++ sistema-flow/src/routes/Remedios/index.tsx
@@ -2,9 +2,9 @@ import CardRemedio from "../../components/Card/CardRemedio";
 
 export default function PaginaRemedios() {
   return (
-    <main className="pagina-remedios">
-      <h1 className="pagina-remedios-titulo">Nossos Remédios</h1>
-      <div className="container-cards">
+    <main>
+      <h1>Nossos Remédios</h1>
+      <div>
         {/* AVISO PARA DEVs DA API: Os remédios devem ser colocados aqui utilizando os tipos presentes em 'remedio.ts' */}
       </div>
     </main>
