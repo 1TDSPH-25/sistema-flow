5cdef7c59a76b39ad46634ae16d0fe5a70d56e00
Felipe-M-de-Sousa <felipe.sousa.1106@gmail.com>
2025-10-15 20:55:42 -0300

    feat: Removendo estilização base do CardRemedio.

--- sistema-flow/src/components/Card/CardRemedio.tsx
+++ sistema-flow/src/components/Card/CardRemedio.tsx
@@ -1,18 +1,18 @@
 import type { Remedio } from "../../types/remedio.ts";
-import './globals.css';
+
 
 export default function CardRemedio({ remedio }: { remedio: Remedio }) {
   return (
-    <div key={remedio.id} className="card-remedio">
-      <img className="card-remedio-imagem" src={remedio.imagem} alt={`Imagem de ${remedio.nome}`} />
-      <div className="card-remedio-corpo">
-        <h2 className="card-remedio-titulo">{remedio.nome}</h2>
-        <p className="card-remedio-descricao">{remedio.descricao}</p>
+    <div key={remedio.id}>
+      <img src={remedio.imagem} alt={`Imagem de ${remedio.nome}`} />
+      <div>
+        <h2>{remedio.nome}</h2>
+        <p>{remedio.descricao}</p>
       </div>
-      <div className="card-remedio-tags">
-        <span className="card-remedio-tag">ID: {remedio.id}</span>
-        <span className="card-remedio-tag">Fab: {remedio.dataFabricacao}</span>
-        <span className="card-remedio-tag">Val: {remedio.dataValidade}</span>
+      <div>
+        <span>ID: {remedio.id}</span>
+        <span>Fab: {remedio.dataFabricacao}</span>
+        <span>Val: {remedio.dataValidade}</span>
       </div>
     </div>
   );
