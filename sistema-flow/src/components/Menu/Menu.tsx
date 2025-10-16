fc6a4600d2696afc3344fc42451764e6ab2e370f
Andrei-Gibbini <andreigibbini52@gmail.com>
2025-10-16 09:01:02 -0300

    feat: adicionando o link para a route de remédios ao menu

--- sistema-flow/src/components/Menu/Menu.tsx
+++ sistema-flow/src/components/Menu/Menu.tsx
@@ -6,6 +6,7 @@ export default function Menu(){
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
             <Link to="/cadastro">Cadastro</Link>
+            <Link to="/remedios">Remedios</Link>
         </nav>
     );
 }
