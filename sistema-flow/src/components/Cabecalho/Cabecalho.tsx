76b36a12fafacae275841d6b1182e9e7c1c3201f
Felipe-M-de-Sousa <felipe.sousa.1106@gmail.com>
2025-10-16 10:34:56 -0300

    feat: alterando a forma com que a function é exportada e adicionando o Menu no cabeçalho.

--- sistema-flow/src/components/Cabecalho/Cabecalho.tsx
+++ sistema-flow/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,10 @@
-export const Cabecalho = () => {
+import Menu from "../Menu/Menu";
+ 
+export default function Cabecalho () {
     return (
     <header>
         <h1>Sistema Flow</h1>
+        < Menu />
     </header>
 )
 };
