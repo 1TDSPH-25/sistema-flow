10443ed0843054204034510fc8255d09478d4ce6
Andrei-Gibbini <andreigibbini52@gmail.com>
2025-10-16 09:22:12 -0300

    feat: removendo o main do App.tsx e adicionando o link para a route Rodape

--- sistema-flow/src/App.tsx
+++ sistema-flow/src/App.tsx
@@ -1,15 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Cabecalho from './components/Cabecalho/Cabecalho';
+import Rodape from './components/Rodape/Rodape';
 // Mantido para estilos globais, se houver.
 
 export default function App() {
   return (
     <div className="app-container">
-      <main>
         <Cabecalho />
         <Outlet />
-      </main>
+        <Rodape />
     </div>
   );
 }
-
