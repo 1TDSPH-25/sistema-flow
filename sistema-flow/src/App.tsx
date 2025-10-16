3e3018253d233e0995893444682379ed88142d5a
manuelalacerda <manuelalacerda88@gmail.com>
2025-10-16 10:09:19 -0300

    feat: Adicionando o link de Cabecalho ao App.tsx

--- sistema-flow/src/App.tsx
+++ sistema-flow/src/App.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import Cabecalho from './components/Cabecalho/Cabecalho';
 // Mantido para estilos globais, se houver.
-
+ 
 export default function App() {
   return (
     <div className="app-container">
       <main>
+        <Cabecalho />
         <Outlet />
       </main>
     </div>
   );
 }
-
