ce04343983901ad2c4425fecb2c1d0e6cdc336ed
Andrei-Gibbini <andreigibbini52@gmail.com>
2025-10-16 09:04:02 -0300

    feat: Adicionando o link de Cabecalho ao App.tsx

--- sistema-flow/src/App.tsx
+++ sistema-flow/src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import Cabecalho from './components/Cabecalho/Cabecalho';
 // Mantido para estilos globais, se houver.
 
 export default function App() {
   return (
     <div className="app-container">
       <main>
+        <Cabecalho />
         <Outlet />
       </main>
     </div>
