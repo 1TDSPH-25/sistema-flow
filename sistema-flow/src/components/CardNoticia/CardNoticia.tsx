8a916dad9a59fa7fa86074ce332fdb9f67f0a0f5
Gdev3356 <gko.starkiller@gmail.com>
2025-10-21 09:13:03 -0300

    feat: estilização dos cards de notícia e o cabeçalho dessa página

--- sistema-flow/src/components/CardNoticia/CardNoticia.tsx
+++ sistema-flow/src/components/CardNoticia/CardNoticia.tsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export default function CardNoticias(props:{noticia: TipoNoticia}){
     return(
         <div className="cardNoticia">
-            <h1>{props.noticia.title}</h1>
-            <p>{props.noticia.description}</p>
-            <Link to={props.noticia.url}>Saiba mais</Link>
-            <img src={props.noticia.urlToImage} alt={props.noticia.title} />
+            <h1 className="noticiaTitulo">{props.noticia.title}</h1>
+            <p className="descricaoNoticia">{props.noticia.description}</p>
+            <Link to={props.noticia.url} className="link">Saiba mais</Link>
+            <img src={props.noticia.urlToImage} alt={props.noticia.title}/>
         </div>
     );
 }
