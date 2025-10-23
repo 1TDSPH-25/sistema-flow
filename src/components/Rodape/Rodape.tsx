b9a98295835867cc7eb43bdd75195c8bfa639c9e
PietroWilhelm <pietrowilhelm@git.com>
2025-10-23 10:54:25 -0300

    bugfix: estilizado o footer

--- src/components/Rodape/Rodape.tsx
+++ src/components/Rodape/Rodape.tsx
@@ -1,6 +1,6 @@
 export default function Rodape(){
     return(
-        <footer>
+        <footer className="flex justify-center gap-6 p-4  bg-blue-600 text-white">
             <p>Sistema Flow - Todos os direitos reservados 2025</p>
         </footer>
     )
