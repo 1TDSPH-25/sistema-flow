bb2e2f1a40d8792c9f9685cd26df43d3a1c141c1
devbelasp <isa.bela360@hotmail.com>
2025-10-23 07:52:40 -0300

    fix: Arrumando o componente Cabecalho.

--- sistema-flow/src/components/Cabecalho/Cabecalho.tsx
+++ sistema-flow/src/components/Cabecalho/Cabecalho.tsx
@@ -1,13 +1,5 @@
 import Menu from "../Menu/Menu";
-<<<<<<< HEAD
- 
-=======
-<<<<<<< HEAD
 
-=======
- 
->>>>>>> dbf2eccdd7cdcd9fed135cfa563e47fe11c6a175
->>>>>>> 2d8c34d52f4ae550a1687082e875df3f9ce32bb2
 export default function Cabecalho () {
     return (
     <header>
