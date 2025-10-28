d60677e9a6f1de1cee996cd68582ef1f02876340
MurilloFernandesCarapia <murilincarapia@gmail.com>
2025-10-28 10:59:19 -0300

    feat(header): integra MenuProvider e HamburgerButton mantendo menu desktop

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,10 +1,19 @@
 import Menu from "../Menu/Menu";
+import { MenuProvider } from "../Menu/context/MenuContext";
+import HamburgerButton from "../Menu/HamburgerButton/HamburgerButton";
 
 export default function Cabecalho () {
     return (
     <header className="flex items-center justify-between p-4 bg-blue-600 text-white">
         <h1>Sistema Flow</h1>
-        < Menu />
+        <MenuProvider>
+            <div className="flex items-center gap-4">
+                <div className="hidden sm:block"><Menu /></div>
+                <div className="sm:hidden">
+                    <HamburgerButton className="text-white" />
+                </div>
+            </div>
+        </MenuProvider>
     </header>
 )
 };
