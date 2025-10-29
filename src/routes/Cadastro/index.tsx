ee1bf60157c14157d75b465adc2efa66c04161c3
PedroPrevitali <pedro.flooperxd2007@gmail.com>
2025-10-28 21:00:56 -0300

    fet: importando dependências

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -1,13 +1,2 @@
-/* Adicionando lembrete no código informando aos devs do menu quais rotas deverão ser criadas e adicionadas ao menu principal:
-scr|
-   | routes |
-            | Login - index.tsx
-            | Cadastro - index.tsx
-
-Outras rotas só devem ser implementadas mediante definição oficial nas tarefas para manter o escopo designado pelo professor. */ 
-
-export default function Cadastro(){
-   return(
-        <h1>Cadastro do Usuário</h1>
-   );
-}
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
