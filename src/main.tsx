f231750f988a879306ee14f12c41bfe4b1c6a686
joaolacerdaconsorte <joaovitorlacerda51@gmail.com>
2025-10-29 15:24:16 -0300

    Corrigindo um erro, não um bugfix, mas para o funcionamento do menu tive que alterar e ajustar config de rota, pois outro integrante da nossa feature alterou e nao estava abrindo com npm run dev, ai ocasionou varios erros e tive que corrigir para conseguir executar.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.tsx'
 import Error from './routes/Error/index.tsx'
 import Login from './routes/Login/index.tsx'
@@ -9,17 +9,18 @@ import Cadastro from './routes/Cadastro/index.tsx'
 import PaginaRemedios from './routes/Remedios/index.tsx'
 import './globals.css'
 
-const router = createBrowserRouter([
-  {path:"/", element: <App/>, errorElement:<Error/>, children:[
-    {path:"/", element:<Home/>}, 
-    {path: "/login", element: <Login/>},
-    {path: "/cadastro", element: <Cadastro/>},
-    {path: "/remedios", element: <PaginaRemedios />}
-  ]}
-], {basename:"/sistema-flow"});
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="cadastro" element={<Cadastro />} />
+          <Route path="remedios" element={<PaginaRemedios />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  </StrictMode>
 )
