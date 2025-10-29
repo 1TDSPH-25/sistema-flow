ca4a792d8096a8c3cb924b067321c4a40371752b
PedroPrevitali <pedro.flooperxd2007@gmail.com>
2025-10-28 21:02:28 -0300

    fet: Inicialização do Formulário com React Hook Form

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -1,3 +1,4 @@
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 export default function PaginaCadastro() {
+   const { register, handleSubmit, formState: { errors } } = useForm()
