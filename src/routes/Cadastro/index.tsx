0bd36ac49b326a6bf7aec4a69caf40a0ae1b9555
PedroPrevitali <pedro.flooperxd2007@gmail.com>
2025-10-28 21:03:02 -0300

    fet: Adicionando uma Função de Envio do Formulário

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -2,3 +2,8 @@ import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 export default function PaginaCadastro() {
    const { register, handleSubmit, formState: { errors } } = useForm()
+
+   function onSubmit(data: any) {
+  console.log(data)
+  alert('Cadastro realizado com sucesso!')
+}
