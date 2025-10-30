e8f28ad424fde0a6a2bedfd2f0d6a0d9e910f4bb
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-10-30 08:16:50 -0300

    fix: Corrigindo o base-name para realiar o deploy no gh-pages.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import Home from './routes/Home/index.tsx'
 import Error from './routes/Error/index.tsx'
@@ -9,18 +9,23 @@ import Cadastro from './routes/Cadastro/index.tsx'
 import PaginaRemedios from './routes/Remedios/index.tsx'
 import './globals.css'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/login', element: <Login /> },
+      { path: '/cadastro', element: <Cadastro /> },
+      { path: '/remedios', element: <PaginaRemedios /> },
+    ],
+  },
+], {basename: import.meta.env.BASE_URL });
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="cadastro" element={<Cadastro />} />
-          <Route path="remedios" element={<PaginaRemedios />} />
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 )
